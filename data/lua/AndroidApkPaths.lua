01902d25cef413b0865027c08315cfb092df1236
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-22 16:02:07 +0800

    Create AndroidApkPaths.lua

---
+++ data/lua/AndroidApkPaths.lua
@@ -0,0 +1,24 @@
+local pack android;
+local pack apk;
+local pack lua;
+local pack haxe;
+local pack json;
+local pack xml;
+local pack paths;
+
+function androidApkPath()
+   apk.androidGetPathSUtil()
+   apkGet.androidPathDir()
+   SUtil.apkGetFilesGetUrl()
+   files.getUrlOpenApk()
+   forAndroidPathsCode()
+   forApkPackPathGet()
+   androidXmlRepath()
+   apk.androidPathJson()
+   apk.androidPathLua()
+   apk.androidPathPng()
+   apk.androidPathJpg()
+   apk.androidPathXml()
+   apk.androidPathfolder()
+   return androidApkPath
+end
