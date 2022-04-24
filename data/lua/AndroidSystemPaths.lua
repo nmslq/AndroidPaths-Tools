3383b533593e4d67dc34d810d5a901b0dbafe0a4
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-24 11:25:18 +0800

    Update AndroidSystemPaths.lua

--- data/lua/AndroidSystemPaths.lua
+++ data/lua/AndroidSystemPaths.lua
@@ -1,9 +1,12 @@
+packages android;
 local pack android;
 local pack systemPath;
 local pack haxe;
 local pack lua;
 local pack json;
 local pack xml;
+local system.io;
+local pack filePath;
 
 function androidPath()
    system.androidGetPathSUtil()
@@ -12,6 +15,9 @@ function androidPath()
    forAndroidPathsCode()
    forSystemPathGet()
    androidXmlRepath()
+   androidSystem.io.toPath()
+   system.io.fileAndroidGetPath()
+   path.theSystemIo.fileAndroid()
    system.androidPathJson()
    system.androidPathLua()
    system.androidPathPng()
